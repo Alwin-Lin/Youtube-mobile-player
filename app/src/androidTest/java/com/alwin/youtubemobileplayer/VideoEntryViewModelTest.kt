f65bdc12615577acf39eeb54b71a9756fa476954
Alwin-Lin <alwin001@gmail.com>
2020-12-30 16:19:56 -0800

    add gradleW for gcp

---
+++ app/src/androidTest/java/com/alwin/youtubemobileplayer/VideoEntryViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.alwin.youtubemobileplayer
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class VideoEntryViewModelTest {
+
+    @Test
+    fun addData() {
+    }
+}
